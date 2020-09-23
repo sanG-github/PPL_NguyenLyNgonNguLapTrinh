116659578cffa8f5537a0b921f49d72a683629c0
Richard <nrcngominh@gmail.com>
2020-09-23 18:52:54 +0700

    update

---
+++ ass1/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+docker build -t ppl-ass1:v1 .
--- ass1/test.sh
+++ ass1/test.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 cd $(dirname $0)
-cd src
 
-echo "START TESTING"
-python3 run.py gen
-python3 run.py test LexerSuite > ../log.lexer.txt
-python3 run.py test ParserSuite > ../log.parser.txt
-echo "DONE"
+docker run -it --rm \
+  -v "$(pwd)/initial:/app" \
+  ppl-ass1:v1 bash test.sh
